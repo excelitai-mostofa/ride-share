896cf699a737c8c91e8cfae33b527358ca1b7bfe
mostofa-mahmud <mostofamahmud23@gmail.com>
2021-12-01 15:38:53 +0600

    3nd

--- lib/tripReview.dart
+++ lib/tripReview.dart
@@ -12,6 +12,28 @@ class _TripPreviewState extends State<TripPreview> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xffcfcfc),
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: CircleAvatar(
+            backgroundColor: Color(0xfffe8550),
+            child: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        ),
+
+        title: Center(
+            child: Text('Trip Preview',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16
+              ),
+            ),
+        ),
+      ),
+
+
       body: Stack(
         children: [
 
@@ -24,8 +46,8 @@ class _TripPreviewState extends State<TripPreview> {
               padding: const EdgeInsets.symmetric(vertical: 80),
 
               child: Container(
-                height: size.height * .5,
-                width: size.width * .9,
+                height: size.height * 0.5,
+                width: size.width * 0.9,
 
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -45,8 +67,7 @@ class _TripPreviewState extends State<TripPreview> {
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                   child: Row(
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    // crossAxisAlignment: CrossAxisAlignment.start,
+
                     children: [
 
 
@@ -65,7 +86,7 @@ class _TripPreviewState extends State<TripPreview> {
 
                             Container(
                               width: 1,
-                              height: 90,
+                              height: 65,
                               decoration: BoxDecoration(
                                   border: Border.all(color: Color(0xffFCF2E7))
                               ),
@@ -77,7 +98,8 @@ class _TripPreviewState extends State<TripPreview> {
                                 backgroundColor: Color(0xffFCF2E7),
                                 child: Icon(Icons.fiber_manual_record_rounded, size: 15,color: Colors.green,),
                               ),
-                            )
+                            ),
+
                           ],
                         ),
                       ),
@@ -88,12 +110,10 @@ class _TripPreviewState extends State<TripPreview> {
                       Expanded(
                         flex: 5,
                         child: Column(
-                          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          //crossAxisAlignment: CrossAxisAlignment.start,
 
                           children: [
                             Expanded(
-                                flex: 1,
+                                flex: 2,
                                 child: SizedBox(
                                   width: MediaQuery.of(context).size.width,
                                   child: Row(
@@ -101,20 +121,33 @@ class _TripPreviewState extends State<TripPreview> {
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
                                       Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
                                         children: [
-                                          Text('Pickup'),
-                                          Text('Location')
+                                          Text('Pickup', style: TextStyle(color: Colors.grey),),
+                                          Text('Location', style: TextStyle(fontWeight: FontWeight.bold),)
                                         ],
                                       ),
-                                      Text('12:06 pm')
+                                      Text('12:06 pm', style: TextStyle(color: Colors.grey),)
                                     ],
                                   ),
                                 )
                             ),
 
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 1,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Color(0xffFCF2E7))
+                              ),
+                            ),
+
+                            SizedBox(
+                              height: 10,
+                            ),
+
 
                             Expanded(
-                                flex: 1,
+                                flex: 3,
                                 child: Row(
                                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -122,41 +155,101 @@ class _TripPreviewState extends State<TripPreview> {
                                     Row(
                                       children: [
                                         Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
                                           children: [
-                                            Text('Pickup'),
-                                            Text('Location')
+                                            Text('Drop off', style: TextStyle(color: Colors.grey),),
+                                            Text('Location', style: TextStyle(fontWeight: FontWeight.bold),),
+                                            SizedBox(height: 8,),
+                                            Text('23 Nov 2021', style: TextStyle(fontWeight: FontWeight.bold),),
+                                            Text('Suzuki Hayate 110', style: TextStyle(color: Colors.grey),),
+
                                           ],
                                         ),
                                       ],
                                     ),
-                                    Text('12:06 pm')
+                                    Column(
+                                      children: [
+                                        Text('12:06 pm', style: TextStyle(color: Colors.grey),),
+                                        SizedBox(height: 15,),
+                                        Text('USD \$47', style: TextStyle(fontWeight: FontWeight.bold),),
+                                        Text('Cash', style: TextStyle(color: Colors.grey),),
+                                      ],
+                                    )
                                   ],
                                 )
+                            ),
+
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 1,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Color(0xffFCF2E7))
+                              ),
+                            ),
+
+                            SizedBox(
+                              height: 10,
                             ),
 
 
 
                             Expanded(
-                                flex: 1,
+                                flex: 2,
                                 child: Row(
                                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Container(
-                                      height: 80,
-                                      width: 90,
-                                      child: Row(
-                                        children: [
-                                          Column(
-                                            children: [
-                                              Text('Pickup'),
-                                              Text('Location')
-                                            ],
-                                          ),
-                                        ],
-                                      ),
+                                    Row(
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text('Trip Cost', style: TextStyle(color: Colors.grey),),
+                                            Text('Voucher', style: TextStyle(color: Colors.grey),),
+                                            Text('Discount', style: TextStyle(color: Colors.grey),),
+                                          ],
+                                        ),
+                                      ],
                                     ),
-                                    Text('12:06 pm')
+                                    Column(
+                                      children: [
+                                        Text('\$47', style: TextStyle(color: Colors.grey),),
+                                        Text('\$47', style: TextStyle(color: Colors.grey),),
+                                        Text('\$47', style: TextStyle(color: Colors.grey),),
+                                      ],
+                                    )
+                                  ],
+                                )
+                            ),
+
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 1,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Color(0xffFCF2E7))
+                              ),
+                            ),
+
+                            SizedBox(
+                              height: 10,
+                            ),
+
+                            Expanded(
+                              flex: 1,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text('Total', style: TextStyle(fontWeight: FontWeight.bold),),
+                                      ],
+                                    ),
+
+                                    Column(
+                                      children: [
+                                        Text('\$47', style: TextStyle(fontWeight: FontWeight.bold),),
+                                      ],
+                                    )
                                   ],
                                 )
                             )
